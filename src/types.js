2c5b82faab59bb19fab50539ba29b0e0390744fd
DURAND Malo <malo.durand@epitech.eu>
2024-05-28 22:41:18 +0200

    feat: adding types folder with User and it's tests

---
+++ src/types.js
@@ -0,0 +1,8 @@
+/**
+ * @author DURAND Malo <malo.durand@epitech.eu>
+ * @description Module for Kook objects' types
+ */
+
+import { User } from "./types/User.js";
+
+export { User };
---
+++ src/types/User.js
@@ -0,0 +1,73 @@
+/**
+ * @author DURAND Malo <malo.durand@epitech.eu>
+ * @description User object : https://developer.kookapp.cn/doc/objects#用户 User
+ */
+
+class User {
+    #raw;
+
+    /**
+     * @param {Object} raw
+     */
+    constructor(raw) {
+        this.#raw = Object.freeze(raw);
+    }
+
+    /**
+     * @return {String}
+     */
+    get id() { return this.#raw.id; }
+
+    /**
+     * @return {String}
+     */
+    get username() { return this.#raw.username; }
+
+    /**
+     * @return {String}
+     */
+    get identify_num() { return this.#raw.identify_num; }
+
+    /**
+     * @return {String}
+     */
+    get avatar() { return this.#raw.avatar; }
+
+    /**
+     * @return {String}
+     */
+    get vip_avatar() { return this.#raw.vip_avatar; }
+
+    /**
+     * @return {String}
+     */
+    get nickname() { return this.#raw.nickname; }
+
+    /**
+     * @brief Returns 0 or 1 for "normal", 10 for "banned"
+     * @return {Number}
+     */
+    get status() { return this.#raw.status; }
+
+    /**
+     * @return {Boolean}
+     */
+    get online() { return this.#raw.online; }
+
+    /**
+     * @return {Boolean}
+     */
+    get bot() { return this.#raw.bot; }
+
+    /**
+     * @return {Boolean}
+     */
+    get mobile_verified() { return this.#raw.mobile_verified; }
+
+    /**
+     * @return {Number[]}
+     */
+    get roles() { return this.#raw.roles; }
+}
+
+export { User };
